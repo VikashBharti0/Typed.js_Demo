6372ca07a226f1f01ac9b86a1b90a175b7468976
vikash bharti <vikasharyan7739@gmail.com>
2025-09-06 22:27:43 +0530

    first commit

---
+++ typejs.js
@@ -0,0 +1,8 @@
+let typed=new Typed('#skills',{
+    strings:[' Core Java','Advance Java','Spring','Spring Boot','HTML','CSS','JavaScript','SQL','Bootstrap'],
+    typeSpeed:50,
+    backSpeed:50,
+    typeDelay:10,
+    backDelay:15,
+    loop:true
+});
